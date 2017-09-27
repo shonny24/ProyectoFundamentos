b61f14a5301105a9069015c601f0069a5d1c2f9a
Jhonny Ocampo <jhonnyokmpo24@hotmail.com>
2017-09-26 23:58:47 -0500

    Se agrego los gets y sets, metodo buscarId y agregarJugador

--- src/logica/OpenDeAustralia.java
+++ src/logica/OpenDeAustralia.java
@@ -10,5 +10,58 @@ package logica;
  * @author Shonny & juank
  */
 public class OpenDeAustralia {
+    private Jugador[] jugadores;
+    private Pista[] pistas;
+
+    public OpenDeAustralia() {
+        this.jugadores = new Jugador[16];
+        this.pistas=new Pista[5];
+    }
+    
+    public boolean agregarJugador(Jugador juga){
+        String cedu=juga.getIdJugador();
+        Jugador verificacion = buscarJugador(cedu);
+        
+        if(verificacion==null){
+            for (int i = 0; i < jugadores.length; i++) {
+                if (jugadores[i] == null) {
+                    jugadores[i] = juga;
+                    break;
+                }
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public Jugador buscarJugador(String idJugador){
+        for (int i = 0; i < jugadores.length; i++) {
+            Jugador p= jugadores[i];
+            if(p!=null){
+                if(idJugador.equals(p.getIdJugador())){
+                    return p;
+                }
+            }
+        }
+        return null;
+    }
+    
+
+    public Jugador[] getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(Jugador[] jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public Pista[] getPistas() {
+        return pistas;
+    }
+
+    public void setPistas(Pista[] pistas) {
+        this.pistas = pistas;
+    }
     
 }
