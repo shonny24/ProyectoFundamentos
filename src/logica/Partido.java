015838b1cbf0f9f1354735b87265d67ef83a255a
Jhonny Ocampo <jhonnyokmpo24@hotmail.com>
2017-09-29 01:28:54 -0500

    Se agrego la clase partido

---
+++ src/logica/Partido.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author Shonny
+ */
+public class Partido {
+    private Jugador jugador1;
+    private Jugador jugador2;
+    private Pista pista;
+    private Estadistica estadistica;
+
+    public Jugador getJugador1() {
+        return jugador1;
+    }
+
+    public void setJugador1(Jugador jugador1) {
+        this.jugador1 = jugador1;
+    }
+
+    public Jugador getJugador2() {
+        return jugador2;
+    }
+
+    public void setJugador2(Jugador jugador2) {
+        this.jugador2 = jugador2;
+    }
+
+    public Pista getPista() {
+        return pista;
+    }
+
+    public void setPista(Pista pista) {
+        this.pista = pista;
+    }
+
+    public Estadistica getEstadistica() {
+        return estadistica;
+    }
+
+    public void setEstadistica(Estadistica estadistica) {
+        this.estadistica = estadistica;
+    }
+
+    public Partido(Jugador jugador1, Jugador jugador2, Pista pista) {
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        this.pista = pista;
+    }
+    
+    
+}
